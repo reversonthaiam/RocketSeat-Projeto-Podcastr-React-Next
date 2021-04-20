39c6d3ae351d41693acf197ede345a0912104f69
reversonthaiam <reversonthayan@gmail.com>
2021-04-20 14:13:31 -0300

    final do dia 2 NLW

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,22 @@
 
-
-export default function Home() {
+export default function Home(props) {
   return (
-    <h1>hello word</h1>
+    <div>
+      <h1>index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   )
+}
+
+export async function getStaticProps(){
+  const response = await fetch('http://localhost:3333/episode')
+  const data = await response.json()
+
+  return{
+    props: {
+      episodes: data, 
+    },
+    revalidate: 60 * 60 * 8,
+    
+  }
 }
