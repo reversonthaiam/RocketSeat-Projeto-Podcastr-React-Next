3fb02534e797b28e1776e211b2dc1b115fb3e86c
reversonthaiam <reversonthayan@gmail.com>
2021-04-21 11:36:49 -0300

    dia 2 parcial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,74 @@
+import { GetStaticProps } from "next";
+import { format, parseISO } from "date-fns";
+import { api } from "../services/api";
+import ptBR from "date-fns/locale/pt-BR";
+import { convertDurationToTimeString } from "../utils/convertDurationToTimeString";
+import styles from './home.module.scss'
 
-export default function Home(props) {
+
+type Episode = {
+  id: string;
+  title: string;
+  thumbnail: string;
+  description: string;
+  members: string;
+  duration: number;
+  durationAsString: string;
+  url: string;
+  publishedAt: string;
+
+};
+
+type HomeProps = {
+  episodes: Episode[];
+};
+
+export default function Home(props: HomeProps) {
   return (
-    <div>
-      <h1>index</h1>
-      <p>{JSON.stringify(props.episodes)}</p>
+    <div className={styles.homepage}>
+      <section className={styles.latestEpisodes}> 
+      <h2>Ultimos lançamentos</h2>
+
+      </section>
+      <section className={styles.allEpisodes}>
+
+      </section>
     </div>
-  )
+  );
 }
 
-export async function getStaticProps(){
-  const response = await fetch('http://localhost:3333/episode')
-  const data = await response.json()
+export const getStaticProps: GetStaticProps = async () => {
+  const {data} = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: "published_at",
+      _order: "desc",
+    },
+  });
 
-  return{
+  const episodes = data.map((episode) => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      published_at: format(parseISO(episode.published_at), "d MMM YY", {
+        locale: ptBR,
+      }),
+      duration: Number(episode.description),
+      durationAsString: convertDurationToTimeString(
+        Number(episode.file.duration)
+      ),
+      description: episode.description,
+      url: episode.file.url,
+    };
+  });
+  
+  return {
     props: {
-      episodes: data, 
+      episodes,
     },
     revalidate: 60 * 60 * 8,
-    
-  }
-}
+  };
+};
+
